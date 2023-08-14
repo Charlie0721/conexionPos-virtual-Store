f7ae16f614717cea1d95d98c32a73e13123ddff4
Carlos Casas <carloscasasramirez@gmail.com>
2023-08-14 11:05:13 -0500

    upload photo-file

--- src/products/components/UploadPhotoFIle.vue
+++ src/products/components/UploadPhotoFIle.vue
@@ -1,61 +1,72 @@
 <template>
     <Navbar />
     <br>
+
     <div class="container mt-4">
+
         <div class="card">
+
             <div class="card-header">
                 Subir Foto del Producto
             </div>
             <div class="card-body">
-                <form @submit.prevent="submitForm">
-                    <div class="mb-3">
-                        <label for="photoInput" class="form-label">Seleccionar foto</label>
-                        <input type="file" class="form-control" id="photoInput" @change="handleFileChange"
-                            accept="image/*" />
-                    </div>
-                    <button type="submit" class="btn btn-primary">Subir Foto</button>
-                </form>
+                <div class="mb-3">
+                    <input type="file" ref="fileInput" class="form-control" @change="handleFileChange" accept="image/*" />
+                </div>
+                <button @click="uploadImage" class="btn btn-primary">Subir Foto</button>
             </div>
+
         </div>
+
     </div>
 </template>
-
-<script lang="ts" setup>
+  
+<script setup lang="ts">
 import { ref } from 'vue';
-import Navbar from '../../components/navbar.vue';
-import { usePhotoFileUpdateStore } from '../stores/update-photo-file.store'
 import { useRoute } from 'vue-router';
-// import Swal from 'sweetalert2';
-const photoFileUpdateStore = usePhotoFileUpdateStore()
+import { URI_API } from '../../services/environmentVariables';
+import axios from 'axios';
+import Navbar from '../../components/navbar.vue'
+import Swal from 'sweetalert2';
 const route = useRoute();
-const productId = ref<number>(0)
-productId.value = Number(route.params.idproducto);
-const selectedFile = ref<File | null>(null);
-
-const handleFileChange = (event: InputEvent) => {
-    const fileInput = event.target as HTMLInputElement;
-    const file = fileInput.files?.[0];
-    selectedFile.value = file || null;
+const productId = ref(Number(route.params.idproducto));
+const fileInput = ref<HTMLInputElement | null>(null);
+const storedToken = localStorage.getItem('authToken');
+const handleFileChange = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    fileInput.value = target;
 };
 
-const submitForm = async () => {
-    if (selectedFile.value) {
-        const productIdPhotoFile = productId.value;
+const uploadImage = async () => {
+    if (fileInput.value && fileInput.value.files && fileInput.value.files.length > 0) {
+        const formData = new FormData();
+        formData.append('file', fileInput.value.files[0]);
         try {
-            const response = await photoFileUpdateStore.uploadFile(productIdPhotoFile, selectedFile.value);
+            const response = await axios.put(`${URI_API}/products/${productId.value}`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Authorization': `Bearer ${storedToken}`,
+                },
+            });
             console.log('Foto subida:', response);
+            return Swal.fire({
+                title: '¡Confirmacion!',
+                text: 'Foto subida satisfactoriamente !',
+                icon: 'success',
+                confirmButtonText: 'Aceptar'
+            })
         } catch (error) {
             console.error('Error al subir la foto:', error);
+            alert(`Error al subir la foto: ${error}`)
         }
+    } else {
+        return Swal.fire({
+            title: '¡Atencion!',
+            text: 'Seleccione un archivo !',
+            icon: 'warning',
+            confirmButtonText: 'Aceptar'
+        })
     }
 };
-
-
-
-
-
-
-
-
-
 </script>
+  
--- src/products/services/upload-photo-product.service.ts
+++
@@ -1,8 +0,0 @@
-import axios from '../../services/axios/axios.connect';
-import { PhotoFileInterface } from '../interfaces/uploadPhotoFile.interface';
-
-export class UploadPhotoFileService {
-  async uploadPhotoService(productId: number, photoFileFormData: FormData) {
-    return await axios.put<PhotoFileInterface>(`/products/${productId}`, photoFileFormData);
-  }
-}
